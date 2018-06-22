9f8d30ca2e22fab87f31a0d8bae4956eadd929e6
Prasad <sumipat500@gmail.com>
2018-06-22 11:13:46 +0530

    cc1

---
+++ intelliq-master/src/main/java/com/intelliq/App.java
@@ -0,0 +1,16 @@
+package com.intelliq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "Hello Maven!" );
+	
+	
+    }
+}
--- maven-master/README.md
+++
@@ -1,4 +0,0 @@
-# maven-project
-
-
-these changes are done by dev and then pushed into git
